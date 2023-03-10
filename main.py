2cf9c19a2a07a5b37b447d485820ffe35b642258
Pydeni <Sonic269@yandex>
2023-03-10 16:32:09 +0300

    все изи

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ root = tree.getroot()
 for i in root.iter('individual'):
     for y in i:
         print(y.tag)
+# Удаляем
+for i in range(4):
+    root[4][0][2][0][0].remove(root[4][0][2][0][0][3])
 
-root[0][4][0][2][0].remove(root[0][4][0][2][0][3])
+
+tree.write('Выписка из реестра 50_04_0000000_13956.xml', encoding='utf-8', xml_declaration=True)
