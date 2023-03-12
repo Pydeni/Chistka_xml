5c5ab593715ef1f0e5949a3a25239e08e835b77b
Pydeni <1CK4pu6NRF>
2023-03-12 16:10:07 +0300

    готово

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import xml.etree.ElementTree as ET
 from openpyxl import load_workbook
+import os
 
-# Парсим хмл
-tree = ET.parse('Выписка из реестра 50_04_0000000_13956.xml')
-root = tree.getroot()
+sp = ['surname','name','patronymic']
+# функция удаления тэгов
+def delit(elem, sp):
+    for el in elem:
+        if el.tag in sp:
+            continue
+        else:
+            elem.remove(el)
+
+#прочитать содержимое
+for f in os.scandir(r'C:\\Users\\Denis\\PycharmProjects\\Chistka_xml\\1'):
+    tree = ET.parse(f.name)
+    root = tree.getroot()
+    # Удаляем координаты
+    for i in root[2]:
+        if i.tag == "contours":
+            root[2].remove(i)
+    # Удаляем личные данные
+    for elem in root.iter('individual'):
+        while True:
+            if len(elem) == len(sp):
+                break
+            else:
+                delit(elem, sp)
+
+    # Перезаписываем xml
+    tree.write(f.name, encoding='utf-8', xml_declaration=True)
 
 
 
-for i in root.iter('individual'):
-    for y in i:
-        print(y.tag)
-# Удаляем
-for i in range(4):
-    root[4][0][2][0][0].remove(root[4][0][2][0][0][3])
 
-
-tree.write('Выписка из реестра 50_04_0000000_13956.xml', encoding='utf-8', xml_declaration=True)
